413d0587b39f7e484e460f2d9b5e2d000231ff83
jussupov <jus.kz09@gmail.com>
2021-05-12 00:58:49 +0600

    fix

--- account/signals.py
+++ account/signals.py
@@ -1,6 +1,6 @@
-from .utils import generate_code
 
 def post_user_save(instance, sender, created, **kwargs):
     if created:
+        from account.utils import generate_code
         generate_code(instance.id, instance.phone)
  
