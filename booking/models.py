51ff680e4e24c683b4ccf33990ac95e41397adc0
Aidyn Jussupov <jussupov@server.tvoe-pole.kz>
2021-06-06 16:09:29 +0600

    fix

--- booking/models.py
+++ booking/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Booking(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     day = models.ManyToManyField(to='utils.DayOfWeek')
-    playground = models.ForeignKey(to='playground.Playground')
-    user = models.ForeignKey(to="account.CustomUser")
-    time_start = models.DateTimeField()
-    time_finish = models.DateTimeField()
-    
+    playground = models.ForeignKey(to='playground.Playground', on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(to="account.CustomUser", on_delete=models.CASCADE, null=True)
+    date_start = models.DateTimeField()
+    date_finish = models.DateTimeField()
