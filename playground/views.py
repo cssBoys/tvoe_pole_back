e6ffe0604b102fde9f4a41a03c220a0dbbb6ac44
jussupov <jus.kz09@gmail.com>
2021-06-06 18:43:38 +0600

    syntax error

--- playground/views.py
+++ playground/views.py
@@ -86,7 +86,7 @@ class PlaygroundViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
             })
         booking_qs = Booking.objects.filter(pk=pk)
         for el in data:
-            datex = date + datetime.timedelta(hour=el['hour'])
+            datex = date + datetime.timedelta(hours=el['hour'])
             if booking_qs.filter(date_start__gte = datex, date_finish__lte = datex).exists():
                 el['active'] = False
         return Response(data)
