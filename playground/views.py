888e77ad2674626ac0c8f58ca290f02a18d1a392
jussupov <jus.kz09@gmail.com>
2021-06-06 19:11:29 +0600

    hours

--- playground/views.py
+++ playground/views.py
@@ -90,7 +90,7 @@ class PlaygroundViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
             })
         booking_qs = Booking.objects.filter(playground_id=pk)
         for el in data:
-            datex = date + datetime.timedelta(hours=el['hour'])
+            datex = date + datetime.timedelta(hours=playground.time_start.hour + el['hour'])
             if booking_qs.filter(date_start__gte = datex, date_finish__lte = datex).exists():
                 el['active'] = False
         return Response(data)
